cc0f65a36eac5de60c772b75549be9931cf1c0df
Jaden <135560395+PaperTop@users.noreply.github.com>
2026-07-02 14:23:19 -0700

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.java
@@ -0,0 +1,44 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        HashMap<Character, Integer> tCount = new HashMap<>();
+        for (char letter : t.toCharArray()) {
+            tCount.put(letter, tCount.getOrDefault(letter, 0) + 1);
+        }
+
+        String res = "";
+        HashMap<Character, Integer> sCount = new HashMap<>();
+        int l = 0;
+        for (int r = 0; r < s.length(); r++) {
+            if (tCount.containsKey(s.charAt(r))) {
+                sCount.put(s.charAt(r), sCount.getOrDefault(s.charAt(r), 0) + 1);
+            }
+            while (isValid(tCount, sCount)) {
+                if (res.equals("") || r - l + 1 < res.length()) {
+                    res = s.substring(l, r + 1);
+                }
+                
+                if (tCount.containsKey(s.charAt(l))) {
+                    if (sCount.get(s.charAt(l)) - 1 == 0) {
+                        sCount.remove(s.charAt(l));
+                    } else {
+                        sCount.put(s.charAt(l), sCount.get(s.charAt(l)) - 1);
+                    }
+                }
+                l++;
+            }
+        }
+
+        return res;
+    }
+
+    public boolean isValid(HashMap<Character, Integer> tCount , HashMap<Character, Integer> sCount) {
+        for (char c : tCount.keySet()) {
+            if (sCount.getOrDefault(c, 0) < tCount.get(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
