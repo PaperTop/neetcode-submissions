e0630a3cbfe9a5aca7ee8fa5e564cb6e44501000
Jaden <135560395+PaperTop@users.noreply.github.com>
2026-07-02 14:34:07 -0700

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.java
@@ -0,0 +1,42 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        HashMap<Character, Integer> tCount = new HashMap<>();
+        for (char letter : t.toCharArray()) {
+            tCount.put(letter, tCount.getOrDefault(letter, 0) + 1);
+        }
+
+        String res = "";
+        HashMap<Character, Integer> sCount = new HashMap<>();
+        int l = 0;
+        int have = 0, need = tCount.size();
+        for (int r = 0; r < s.length(); r++) {
+            if (tCount.containsKey(s.charAt(r))) {
+                sCount.put(s.charAt(r), sCount.getOrDefault(s.charAt(r), 0) + 1);
+                if (sCount.get(s.charAt(r)).equals(tCount.get(s.charAt(r)))) {
+                    have++;
+                }
+            }
+            while (have == need) {
+                if (res.equals("") || r - l + 1 < res.length()) {
+                    res = s.substring(l, r + 1);
+                }
+                
+                if (tCount.containsKey(s.charAt(l))) {
+                    if (sCount.get(s.charAt(l)) - 1 == 0) {
+                        sCount.remove(s.charAt(l));
+                        have--;
+                    } else {
+                        sCount.put(s.charAt(l), sCount.get(s.charAt(l)) - 1);
+                        if (sCount.get(s.charAt(l)) < tCount.get(s.charAt(l))) {
+                            have--;
+                        }
+                    }
+                    
+                }
+                l++;
+            }
+        }
+
+        return res;
+    }
+}
