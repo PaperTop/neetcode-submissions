4f2cda72b84ce111dda7bb35a30b4b74f57f888b
Jaden <135560395+PaperTop@users.noreply.github.com>
2026-08-12 14:19:47 -0700

    Add: kth-largest-integer-in-a-stream - submission-1

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-1.java
@@ -0,0 +1,19 @@
+class KthLargest {
+
+    private int k;
+    private List<Integer> arr = new ArrayList<>();
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        for (int num : nums) {
+            arr.add(num);
+        }
+    }
+    
+    public int add(int val) {
+        arr.add(val);
+        Collections.sort(arr);
+
+        return arr.get(arr.size() - k);
+    }
+}
