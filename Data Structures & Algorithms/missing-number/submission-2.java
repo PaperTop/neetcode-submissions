270ea4ae8defe15245d2c93c15f77c28e5da2518
Jaden <135560395+PaperTop@users.noreply.github.com>
2026-08-22 00:11:32 -0700

    Add: missing-number - submission-2

---
+++ Data Structures & Algorithms/missing-number/submission-2.java
@@ -0,0 +1,10 @@
+//improvements
+class Solution {
+    public int missingNumber(int[] nums) {
+        int res = nums.length;
+        for (int i = res - 1; i >= 0; i--) {
+            res += i - nums[i];
+        } 
+        return res;
+    }
+}
