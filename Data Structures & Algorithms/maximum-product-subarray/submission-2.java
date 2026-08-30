af6329b5ef60314d7a6f98b53b1ee9392c381139
Jaden <135560395+PaperTop@users.noreply.github.com>
2026-08-29 23:12:11 -0700

    Add: maximum-product-subarray - submission-2

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int n = nums.length;
+        int res = nums[0];
+        int prefix = 0, suffix = 0;
+        /* 
+            Why prefix and suffix? Why two?
+            Q: Odd number of negative numbers -> A: alternate which one to use.
+        */
+
+        for (int i = 0; i < n; i++) {
+            prefix = nums[i] * (prefix == 0 ? 1 : prefix);
+            suffix = nums[n - i - 1] * (suffix == 0 ? 1 : suffix);
+            res = Math.max(res, Math.max(prefix, suffix));
+        }
+
+        return res;
+    }
+}
