cd40aa3cdfb2e4eeb3b7936d01aa2dcb45c816ed
Jaden <135560395+PaperTop@users.noreply.github.com>
2026-09-06 22:48:57 -0700

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int i = digits.length - 1;
+        int carry = 1;
+        while (carry > 0 && i >= 0) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            } else {
+                digits[i] += 1;
+                carry = 0;
+            }
+            i--;
+        }
+
+        if (carry == 1 && i == -1) {
+            int[] big = new int[digits.length + 1];
+            big[0] = 1;
+            for (int idx = 1; idx < big.length; idx++) {
+                big[idx] = 0;
+            }
+            return big;
+        }
+
+        return digits;
+    }
+}
