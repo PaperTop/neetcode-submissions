31319cd5a4aead5d5f263da8687bfcd95d33eb00
Jaden <135560395+PaperTop@users.noreply.github.com>
2026-09-12 19:10:54 -0700

    Add: count-squares - submission-0

---
+++ Data Structures & Algorithms/count-squares/submission-0.java
@@ -0,0 +1,40 @@
+class CountSquares {
+    Map<Integer, Map<Integer, Integer>> rowToCols;
+    Map<Integer, Map<Integer, Integer>> colToRows;
+
+    public CountSquares() {
+        rowToCols = new HashMap<>();
+        //colToRows = new HashMap<>();
+    }
+    
+    public void add(int[] point) {
+        int x = point[0];
+        int y = point[1];
+
+        rowToCols.computeIfAbsent(x, k -> new HashMap<>()).put(y, rowToCols.get(x).getOrDefault(y, 0) + 1);
+        //colToRows.computeIfAbsent(y, k -> new HashMap<>()).put(x, colToRows.get(y).getOrDefault(x, 0) + 1);
+    }
+    
+    public int count(int[] point) {
+        if (!rowToCols.containsKey(point[0])) return 0;
+        int squares = 0;
+        int x = point[0];
+        int y = point[1];
+        
+        for (int newY : rowToCols.get(x).keySet()) {
+            if (newY == y) continue;
+            int dist = Math.abs(newY - y);
+
+            int side = rowToCols.get(x).get(newY);
+            int sideUp = rowToCols.containsKey(x - dist) ? rowToCols.get(x - dist).getOrDefault(newY, 0) : 0;
+            int sideDown = rowToCols.containsKey(x + dist) ? rowToCols.get(x + dist).getOrDefault(newY, 0) : 0;
+            int up = rowToCols.containsKey(x - dist) ? rowToCols.get(x - dist).getOrDefault(y, 0) : 0;
+            int down = rowToCols.containsKey(x + dist) ? rowToCols.get(x + dist).getOrDefault(y, 0) : 0;
+
+            //Check square upward and downward
+            squares += side * sideUp * up + side * sideDown * down;
+        }
+
+        return squares;
+    }
+}
