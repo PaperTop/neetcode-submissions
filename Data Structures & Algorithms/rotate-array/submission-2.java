3b590baac529b4d62a82de736bfec30edb1822a7
Jaden <135560395+PaperTop@users.noreply.github.com>
2026-09-18 22:27:01 -0700

    Add: rotate-array - submission-2

---
+++ Data Structures & Algorithms/rotate-array/submission-2.java
@@ -0,0 +1,21 @@
+//Strat: just shift one thing at a time to the right and if you hit the same start iterate to the next one and shift that line.
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k %= n;
+        int count = 0;
+
+        for (int start = 0; count < n; start++) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                int nextIdx = (current + k) % n;
+                int temp = nums[nextIdx];
+                nums[nextIdx] = prev;
+                prev = temp;
+                current = nextIdx;
+                count++;
+            } while (start != current);
+        }
+    }
+}
