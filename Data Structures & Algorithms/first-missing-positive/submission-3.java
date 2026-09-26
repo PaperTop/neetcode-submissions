31258d93c21c85e2ecb59923e8a29d450963ae20
Jaden <135560395+PaperTop@users.noreply.github.com>
2026-09-25 18:44:50 -0700

    Add: first-missing-positive - submission-3

---
+++ Data Structures & Algorithms/first-missing-positive/submission-3.java
@@ -0,0 +1,22 @@
+/*
+Insight: smallest possible is 1 and the largest possible is n + 1, we can use the array nums itself to store if that number exist at that current idx and use some special opertions to ensure we don't lose a number at its new position by swapping until we can no longer
+*/ 
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] > 0 && nums[i] < nums.length + 1 && nums[i] != i + 1 && nums[nums[i] - 1] != nums[i]) {
+                int tmp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = tmp;
+            }
+        }   
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return nums.length + 1;
+    }
+}
